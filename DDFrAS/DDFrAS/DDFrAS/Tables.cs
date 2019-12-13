0b92699afa8344fdcf3bc3862cec1497b03124a2
Tjeerd de Jager <tjeerd.dejager@outlook.com>
2019-12-13 11:42:14 +0100

    Database III

--- DDFrAS/DDFrAS/DDFrAS/Tables.cs
+++ DDFrAS/DDFrAS/DDFrAS/Tables.cs
@@ -1,58 +1,96 @@
 ﻿using System;
 public class POP
 {
-    public int Pop_ID {get; set;}
+    //PK
+    public int Pop_ID { get; set; }
+
+    //Other column
     public string Pop_Name { get; set; }
-
 }
 
-public class SWTICH
+public class SWITCH
 {
+    //PK
     public int Switch_ID { get; set; }
-    public int POP_ID { get; set; }
+
+    //Other column
     public string Switch_Name { get; set; }
+
+    //FK
+    public int POP_ID { get; set; }
+    public POP POP { get; set; }
+
+    //FK
+    public int 
 }
-//public class PORT
-//{
-//    public int Port_ID { get; set; }
-//    public int Switch_ID { get; set; }
-//    public string PortName { get; set; }
-//}
 
-//public class SSH_COMMANDS
-//{
-//    public int Command_ID { get; set; }
-//    public string Command_Name { get; set; }
-//    public string Command_Temp { get; set; }
-//}
+public class PORT
+{
+    //PK
+    public int Port_ID { get; set; }
 
-//public class PRODUCT
-//{
-//    public int Product_ID { get; set; }
-//    public int Pop_ID { get; set; }
-//    public string Product_Name { get; set; }
-//}
+    //Other column
+    public string PortName { get; set; }
 
-//public class CONFIG
-//{
-//    public int Config_ID { get; set; }
-//    public int Poort_ID { get; set; }
-//    public int Product_ID { get; set; }
-//    public int Command_ID { get; set; }
-//    public int Line_ID { get; set; }
-//    public int Status { get; set; }
-//    public DateTime ExDate { get; set; }
-//    public string Script { get; set; }
-//}
+    //FK
+    public int Switch_ID { get; set; }
+    public SWITCH SWITCH { get; set; }
 
-//public class CONFIG_VAR
-//{
-//    public int Conf_var_ID;
-//    public int Config_ID;
-//    public int Vlan;
-//    public string Man_IP;
-//    public string Usernamessh;
-//    public string PasswordSSH;
-//    public string Password;
-//    public string Terminal;
-//}
+}
+
+public class SSH_COMMANDS
+{
+    //PK
+    public int Command_ID { get; set; }
+
+    //Other column
+    public string Command_Name { get; set; }
+    public string Command_Temp { get; set; }
+}
+
+public class PRODUCT
+{
+    //PK
+    public int Product_ID { get; set; }
+
+    //Other column
+    public string Product_Name { get; set; }
+}
+
+public class CONFIG
+{
+    //PK
+    public int Config_ID { get; set; }
+
+    //Other column
+    public int Line_ID { get; set; }
+    public int Status { get; set; }
+    public DateTime ExDate { get; set; }
+    public string Script { get; set; }
+
+    //FK
+    public int Port_ID { get; set; }
+    public PORT PORT { get; set; }
+    public int Product_ID { get; set; }
+    public PRODUCT PRODUCT { get; set; }
+    public int Command_ID { get; set; }
+    public SSH_COMMANDS SSH_COMMAND { get; set; }
+
+}
+
+public class CONFIG_VAR
+{
+    //PK
+    public int Conf_var_ID;
+
+    //Other column
+    public int Vlan;
+    public string Man_IP;
+    public string Usernamessh;
+    public string Passwordssh;
+    public string Term_pass;
+
+    //FK
+    public int Config_ID;
+    public CONFIG CONFIG;
+}
