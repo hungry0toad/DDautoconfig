e575eaedcb4b65405f69dc69655168b69c97b745
Tjeerd de Jager <tjeerd.dejager@outlook.com>
2019-12-19 10:51:11 +0100

    Database V and Start request form variables script

---
+++ DDFrAS/DDFrAS/DDFrAS/Newvars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDFrAS.DDFrAS
+{
+    public class Newvdbinputs
+    {
+        public static void Newvars(string vlan, string manip, string username, string passwordssh, string terminal)
+        {
+            using (DDFrASEntities context = new DDFrASEntities())
+            {
+                CONFIG_VAR configvar = new CONFIG_VAR
+                {
+                    Vlan = vlan,
+                    Man_IP = manip,
+                    Username = username,
+                    PasswordSSH = passwordssh,
+                    Terminal = terminal
+                };
+                context.CONFIG_VAR.Add(configvar);
+                context.SaveChanges();
+            }
+        }
+    }
+}
